65880f5c61f24191e515a72ad661a5ca52a33fd4
Andrew Appleton <andrew@floatleft.com>
2015-04-18 17:49:34 +0100

    Notes show

---
+++ app/pods/notes/note/controller.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  note: Ember.computed.alias('model'),
+
+  saveNote() {
+    if (this.get('note.isDirty')) {
+      return this.get('note').set('updatedAt', new Date()).save();
+    }
+  },
+
+  observeNoteText: function() {
+    Ember.run.debounce(this, this.saveNote, 1000);
+  }.observes('note.text'),
+
+  actions: {
+    deleteNote() {
+      this.get('note').destroyRecord().then(() => this.transitionToRoute('notes'));
+    }
+  }
+})
--- app/pods/notes/note/route.js
+++ app/pods/notes/note/route.js
@@ -3,5 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(params) {
     return this.store.find('note', params.id);
+  },
+
+  renderTemplate(...args) {
+    this.render('notes.note.actions', { outlet: 'app-header' });
+    return this._super(...args);
   }
 });
