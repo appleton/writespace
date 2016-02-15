8c20bb66e8907d6e2e28cd2a63f4ba16d8dcd0d8
Andrew Appleton <andrew@floatleft.com>
2016-02-15 09:43:49 +0000

    Add offline states for user pages

---
+++ app/pods/user/error/route.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+const { on } = Ember;
+
+export default Ember.Route.extend({
+  listenForOnline: on('activate', function() {
+    this._onlineListener = window.addEventListener('online', function() {
+      this.transitionTo('user.login');
+    }.bind(this));
+  }),
+
+  cancelListenForOnline: on('deactivate', function() {
+    window.removeEventListener(this._onlineListener);
+  })
+});
--- app/pods/user/login/route.js
+++ app/pods/user/login/route.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
 import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
+import OnlineOnlyRoute from 'scribly/mixins/online-only-route';
 
-export default Ember.Route.extend(UnauthenticatedRouteMixin);
+export default Ember.Route.extend(UnauthenticatedRouteMixin, OnlineOnlyRoute);
