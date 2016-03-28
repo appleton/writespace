5446f20fca02e17a741aa806d35d20a4b0450100
Andrew Appleton <andrew@floatleft.com>
2016-03-28 23:48:08 +0100

    assert against request

--- tests/acceptance/user/login-test.js
+++ tests/acceptance/user/login-test.js
@@ -5,6 +5,34 @@ import page from 'writespace/tests/pages/user/login';
 
 moduleForAcceptance('Acceptance | user/login');
 
+test('success', function(assert) {
+  assert.expect(2);
+  const done = assert.async();
+
+  server.post('/db/_session', (db, request) => {
+    assert.equal(JSON.parse(request.requestBody).name, 'test@example.com');
+    assert.equal(JSON.parse(request.requestBody).password, 's3cret');
+
+    done();
+
+    return {
+      ok: true,
+      name: 'test@example.com',
+      roles: []
+    };
+  }, 201);
+
+  page
+    .visit()
+    .email('test@example.com')
+    .password('s3cret')
+    .submit();
+
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+  });
+});
+
 test('error', function(assert) {
   server.get('/db/_session', {}, 404);
   server.post('/db/_session', {
@@ -20,17 +48,5 @@ test('error', function(assert) {
 
   andThen(function() {
     assert.equal(page.errors, 'Name or password is incorrect.');
-  });
-});
-
-test('success', function(assert) {
-  page
-    .visit()
-    .email('test@example.com')
-    .password('s3cret')
-    .submit();
-
-  andThen(function() {
-    assert.equal(currentURL(), '/');
   });
 });
